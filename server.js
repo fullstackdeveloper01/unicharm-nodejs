7c9e18ef79024b0df787ff38125220029a60485a
fullstackdeveloper <73298382+fullstackdeveloper01@users.noreply.github.com>
2026-01-22 15:23:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const db = require('./models');
 const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors());
