ca99eee97efc275ce83c15ce825b2da73d40fa1c
durva <durvasadawarte178@gmail.com>
2026-02-02 16:19:00 +0530

    fixedddd

--- routes/superadmin/meetingRequest.routes.js
+++ routes/superadmin/meetingRequest.routes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const meetingRequestController = require('../../controllers/superadmin/meetingRequest.controller');
+const meetingRequestController = require('../../controllers/superadmin/meetingRequestController');
 
 const { verifyToken } = require('../../middlewares/shared/auth');
 
 // GET meeting requests list with pagination & search
-router.get('/', verifyToken, meetingRequestController.getMeetingRequests);
+router.get('/', verifyToken, meetingRequestController.getAllRequests);
 
 // PUT update meeting request
-router.put('/:id', meetingRequestController.updateMeetingRequest);
+router.put('/:id', meetingRequestController.updateRequest);
 
 // DELETE meeting request
-router.delete('/:id', meetingRequestController.deleteMeetingRequest);
+router.delete('/:id', meetingRequestController.deleteRequest);
 
 module.exports = router;
