1c4ee7d79a9466a6aab80700e6e493d37b6ccd1a
durva <durvasadawarte178@gmail.com>
2026-02-05 16:00:19 +0530

    updated profile

--- routes/employee/profile.routes.js
+++ routes/employee/profile.routes.js
@@ -8,8 +8,13 @@ router.use(verifyToken);
 
 router.get('/', profileController.getProfile);
 router.post('/update', profileController.uploadMiddleware, profileController.updateProfile);
-router.put('/update', profileController.uploadMiddleware, profileController.updateProfile); // Alias for PUT
-router.put('/', profileController.uploadMiddleware, profileController.updateProfile); // Alias for RESTful update
+router.put('/update', profileController.uploadMiddleware, profileController.updateProfile);
+router.patch('/update', profileController.uploadMiddleware, profileController.updateProfile);
+
+// Root path handlers
+router.post('/', profileController.uploadMiddleware, profileController.updateProfile);
+router.put('/', profileController.uploadMiddleware, profileController.updateProfile);
+router.patch('/', profileController.uploadMiddleware, profileController.updateProfile);
 router.post('/change-password', profileController.changePassword);
 
 module.exports = router;
--- routes/superadmin/profileRoutes.js
+++ routes/superadmin/profileRoutes.js
@@ -5,8 +5,13 @@ const profileController = require('../../controllers/superadmin/profileControlle
 // Routes for SuperAdmin Profile
 router.get('/', profileController.getProfile);
 router.post('/update', profileController.uploadMiddleware, profileController.updateProfile);
-router.put('/update', profileController.uploadMiddleware, profileController.updateProfile); // Alias for PUT
-router.put('/', profileController.uploadMiddleware, profileController.updateProfile); // Alias for RESTful update
+router.put('/update', profileController.uploadMiddleware, profileController.updateProfile);
+router.patch('/update', profileController.uploadMiddleware, profileController.updateProfile);
+
+// Root path handlers
+router.post('/', profileController.uploadMiddleware, profileController.updateProfile);
+router.put('/', profileController.uploadMiddleware, profileController.updateProfile);
+router.patch('/', profileController.uploadMiddleware, profileController.updateProfile);
 router.post('/change-password', profileController.changePassword);
 
 module.exports = router;
